50559bbc6599ed4c3339ef5601f821cb65f857db
David Cana <david.javapagetemplates@gmail.com>
2017-10-23 13:54:56 +0200

    delete test added to index.js.

--- test/js/app/index.js
+++ test/js/app/index.js
@@ -2,5 +2,6 @@ QUnit.testSuites([
     { name: "paging Test Suite", path: "./paging.html" },
     { name: "sorting Test Suite", path: "./sorting.html" },
     { name: "filtering Test Suite", path: "./filtering.html" },
-    { name: "selecting Test Suite", path: "./selecting.html" }
+    { name: "selecting Test Suite", path: "./selecting.html" },
+    { name: "delete Test Suite", path: "./delete.html" }
 ]);
