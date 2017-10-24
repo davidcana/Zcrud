a3a13bb9c4ed93af07fe04988b21771c0a97f647
David Cana <david.javapagetemplates@gmail.com>
2017-10-24 13:24:07 +0200

    create test added to index.js.

--- test/js/app/index.js
+++ test/js/app/index.js
@@ -3,5 +3,6 @@ QUnit.testSuites([
     { name: "sorting Test Suite", path: "./sorting.html" },
     { name: "filtering Test Suite", path: "./filtering.html" },
     { name: "selecting Test Suite", path: "./selecting.html" },
-    { name: "delete Test Suite", path: "./delete.html" }
+    { name: "delete Test Suite", path: "./delete.html" },
+    { name: "create Test Suite", path: "./create.html" }
 ]);
