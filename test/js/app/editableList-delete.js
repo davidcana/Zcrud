f3676606681a3cd22e49af8e6292292e4eed87d0
David Cana <david.javapagetemplates@gmail.com>
2017-11-23 12:41:46 +0100

    editableList-delete finished.

--- test/js/app/editableList-delete.js
+++ test/js/app/editableList-delete.js
@@ -23,7 +23,6 @@ $( '#departmentsContainer' ).zcrud(
     function( options ){
         
         // Run tests
-        
         QUnit.test( "delete test", function( assert ) {
             
             testUtils.resetServices();
@@ -62,52 +61,32 @@ $( '#departmentsContainer' ).zcrud(
             
             testHelper.checkNoRecord( assert, key );
         });
-        /*
-        QUnit.test( "change with errors test", function( assert ) {
+        
+        QUnit.test( "delete 3 rows test", function( assert ) {
 
             testUtils.resetServices();
             fatalErrorFunctionCounter = 0;
             $( '#departmentsContainer' ).zcrud( 'load' );
 
-            var editable = true;
-
-            // Assert register with key 5 exists
-            var key = 5;
-            var record =  {
-                "id": "" + key,
-                "name": "Service " + key
-            };
-            testHelper.checkRecord( assert, key, record, editable );
-            var values = testHelper.buildCustomValuesList( testHelper.buildValuesList( 1, 10 ) );
-            testHelper.pagingTest({
-                options: options,
-                assert: assert,
-                visibleRows: 10,
-                pagingInfo: 'Showing 1-10 of 129',
-                ids:  values[ 0 ],
-                names: values[ 1 ],
-                pageListNotActive: [ '<<', '<', '1' ],
-                pageListActive: [ '2', '3', '4', '5', '13', '>', '>>' ],
-                editable: editable
-            });
-
-            // Edit record
-            var editedRecord =  {
-                "name": "Service 5 edited",
-                "number": "a"
-            };
-            testHelper.fillEditableList( editedRecord, key );
-            var newRecord = $.extend( true, {}, record, editedRecord );
-            testHelper.checkEditableListForm( assert, key, newRecord );
+            // Delete records
+            var key1 = 3;
+            testHelper.clickDeleteRowListButton( key1 );
+            var key2 = 5;
+            testHelper.clickDeleteRowListButton( key2 );
+            var key3 = 7;
+            testHelper.clickDeleteRowListButton( key3 );
             
+            // Save
             assert.equal( fatalErrorFunctionCounter, 0 );
             testHelper.clickEditableListSubmitButton();
-            assert.equal( fatalErrorFunctionCounter, 1 );
-            
-            testHelper.checkRecord( assert, key, record, editable, true );
+            assert.equal( fatalErrorFunctionCounter, 0 );
+
+            testHelper.checkNoRecord( assert, key1 );
+            testHelper.checkNoRecord( assert, key2 );
+            testHelper.checkNoRecord( assert, key3 );
         });
         
-        QUnit.test( "change undo/redo 1 action test", function( assert ) {
+        QUnit.test( "delete undo/redo 1 action test", function( assert ) {
 
             testUtils.resetServices();
             fatalErrorFunctionCounter = 0;
@@ -135,15 +114,9 @@ $( '#departmentsContainer' ).zcrud(
                 editable: editable
             });
 
-            // Edit record
-            var editedRecord =  {
-                "name": "Service 2 edited"
-            };
-            testHelper.fillEditableList( editedRecord, key );
-            var newRecord = $.extend( true, {}, record, editedRecord );
-            testHelper.checkEditableListForm( assert, key, newRecord );
-            testHelper.assertHistory( assert, 1, 0, true );
-            
+            // Delete record
+            testHelper.clickDeleteRowListButton( key );
+
             // Undo
             testHelper.clickUndoButton();
             testHelper.checkRecord( assert, key, record, editable );
@@ -151,125 +124,82 @@ $( '#departmentsContainer' ).zcrud(
             
             // Redo
             testHelper.clickRedoButton();
-            testHelper.checkEditableListForm( assert, key, newRecord );
+            testHelper.checkEditableListForm( assert, key, record );
             testHelper.assertHistory( assert, 1, 0, true );
             
             // Save
             assert.equal( fatalErrorFunctionCounter, 0 );
             testHelper.clickEditableListSubmitButton();
             assert.equal( fatalErrorFunctionCounter, 0 );
-            testHelper.assertHistory( assert, 0, 0, false );
 
-            testHelper.checkRecord( assert, key, newRecord, editable );
+            testHelper.checkNoRecord( assert, key );
         });
         
-        QUnit.test( "change undo/redo 3 actions test", function( assert ) {
+        QUnit.test( "delete undo/redo 3 actions test", function( assert ) {
 
             testUtils.resetServices();
             fatalErrorFunctionCounter = 0;
             $( '#departmentsContainer' ).zcrud( 'load' );
-
+            
             var editable = true;
-
-            // Assert register with key 2 exists
-            var key1 = 2;
+            
+            // Delete records
+            var key1 = 3;
             var record1 =  {
                 "id": "" + key1,
                 "name": "Service " + key1
             };
-            testHelper.checkRecord( assert, key1, record1, editable );
-            var values = testHelper.buildCustomValuesList( testHelper.buildValuesList( 1, 10 ) );
-            testHelper.pagingTest({
-                options: options,
-                assert: assert,
-                visibleRows: 10,
-                pagingInfo: 'Showing 1-10 of 129',
-                ids:  values[ 0 ],
-                names: values[ 1 ],
-                pageListNotActive: [ '<<', '<', '1' ],
-                pageListActive: [ '2', '3', '4', '5', '13', '>', '>>' ],
-                editable: editable
-            });
-
-            // Edit record
-            var editedRecord =  {
-                "name": "Service " + key1 + " edited"
-            };
-            testHelper.fillEditableList( editedRecord, key1 );
-            var newRecord1 = $.extend( true, {}, record1, editedRecord );
-            testHelper.checkEditableListForm( assert, key1, newRecord1 );
-            
-            testHelper.assertHistory( assert, 1, 0, true );
-            
-            // Edit record (2)
-            var key2 = 4;
+            testHelper.clickDeleteRowListButton( key1 );
+            var key2 = 5;
             var record2 =  {
                 "id": "" + key2,
                 "name": "Service " + key2
             };
-            var editedRecord2 =  {
-                "name": "Service " + key2 + " edited"
-            };
-            testHelper.fillEditableList( editedRecord2, key2 );
-            var newRecord2 = $.extend( true, {}, record2, editedRecord2 );
-            testHelper.checkEditableListForm( assert, key2, newRecord2 );
-            
-            testHelper.assertHistory( assert, 2, 0, true );
-            
-            // Edit record (3)
+            testHelper.clickDeleteRowListButton( key2 );
             var key3 = 7;
             var record3 =  {
                 "id": "" + key3,
                 "name": "Service " + key3
             };
-            var editedRecord3 =  {
-                "name": "Service " + key3 + " edited"
-            };
-            testHelper.fillEditableList( editedRecord3, key3 );
-            var newRecord3 = $.extend( true, {}, record3, editedRecord3 );
-            testHelper.checkEditableListForm( assert, key3, newRecord3 );
-            
-            testHelper.assertHistory( assert, 3, 0, true );
-            
+            testHelper.clickDeleteRowListButton( key3 );
+
             // Undo (1)
             testHelper.clickUndoButton();
             testHelper.checkRecord( assert, key3, record3, editable );
             testHelper.assertHistory( assert, 2, 1, true );
-            
+
             // Undo (2)
             testHelper.clickUndoButton();
             testHelper.checkRecord( assert, key2, record2, editable );
             testHelper.assertHistory( assert, 1, 2, true );
-            
+
             // Undo (3)
             testHelper.clickUndoButton();
             testHelper.checkRecord( assert, key1, record1, editable );
             testHelper.assertHistory( assert, 0, 3, false );
-            
+
             // Redo (1)
             testHelper.clickRedoButton();
-            testHelper.checkEditableListForm( assert, key1, newRecord1 );
+            testHelper.checkEditableListForm( assert, key1, record1 );
             testHelper.assertHistory( assert, 1, 2, true );
-            
+
             // Redo (2)
             testHelper.clickRedoButton();
-            testHelper.checkEditableListForm( assert, key2, newRecord2 );
+            testHelper.checkEditableListForm( assert, key2, record2 );
             testHelper.assertHistory( assert, 2, 1, true );
-            
+
             // Redo (3)
             testHelper.clickRedoButton();
-            testHelper.checkEditableListForm( assert, key3, newRecord3 );
+            testHelper.checkEditableListForm( assert, key3, record3 );
             testHelper.assertHistory( assert, 3, 0, true );
             
             // Save
             assert.equal( fatalErrorFunctionCounter, 0 );
             testHelper.clickEditableListSubmitButton();
             assert.equal( fatalErrorFunctionCounter, 0 );
-            testHelper.assertHistory( assert, 0, 0, false );
 
-            testHelper.checkRecord( assert, key1, newRecord1, editable );
-            testHelper.checkRecord( assert, key2, newRecord2, editable );
-            testHelper.checkRecord( assert, key3, newRecord3, editable );
+            testHelper.checkNoRecord( assert, key1 );
+            testHelper.checkNoRecord( assert, key2 );
+            testHelper.checkNoRecord( assert, key3 );
         });
-        */
     });
--- test/js/app/index.js
+++ test/js/app/index.js
@@ -1,3 +1,6 @@
+
+QUnit.config.reorder = false;
+
 QUnit.testSuites([
     { name: "paging Test Suite", path: "./paging.html" },
     { name: "sorting Test Suite", path: "./sorting.html" },
@@ -5,5 +8,8 @@ QUnit.testSuites([
     { name: "selecting Test Suite", path: "./selecting.html" },
     { name: "delete Test Suite", path: "./delete.html" },
     { name: "create Test Suite", path: "./create.html" },
-    { name: "update Test Suite", path: "./update.html" }
+    { name: "update Test Suite", path: "./update.html" },
+    { name: "editable list change Test Suite", path: "./editableList-change.html" },
+    { name: "editable list create Test Suite", path: "./editableList-create.html" },
+    { name: "editable list delete Test Suite", path: "./editableList-delete.html" }
 ]);
