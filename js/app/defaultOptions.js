270f6118a2d367e351119e4938dd07396bb1e396
David Cana <david.javapagetemplates@gmail.com>
2018-04-24 18:28:08 +0200

    main.js refactored: defaultOptions.js and normalizer.js make main.js simpler.

---
+++ js/app/defaultOptions.js
@@ -0,0 +1,198 @@
+"use strict";
+
+var $ = require( 'jquery' );
+var log4javascript = require( 'log4javascript' );
+
+module.exports = {
+
+    validation: {
+        modules: '',
+        rules: {},
+        configuration: {
+            errorMessageClass: 'form-error-inline-absolute', // form-error-fixed, form-error-inline-static and form-error-inline-absolute already exist
+            borderColorOnError: ''
+        }
+    },
+    dictionary: {},
+
+    saveUserPreferences: true,
+    body: document.body,
+    entityId: 'entity',
+
+    fields: {},
+    fieldsConfig: {
+        defaultFieldOptions: {
+            datetime: {
+                inline: false,
+                minYear: 1950,
+                maxYear: 2050,
+                maxHour: 23,
+                minutesStep: 5,
+                timerDelay: 100
+            },
+            date: {
+                inline: false,
+                minYear: 1950,
+                maxYear: 2050
+            },
+            time: {
+                inline: false,
+                maxHour: 99,
+                minutesStep: 5,
+                timerDelay: 100
+            }
+        },
+        getDefaultFieldTemplate: function( field ){
+            return field.type + '@templates/fields/basic.html';
+        }
+    },
+
+    events: {
+        formClosed: function ( data, event ) {},
+        formCreated: function ( data ) {},
+        formSubmitting: function ( data, event ) {},
+        recordAdded: function ( data, event ) {},
+        recordDeleted: function ( data, event ) {},
+        recordUpdated: function ( data, event ) {},
+        selectionChanged: function ( data ) {}
+    },
+
+    pages: {
+        list: {
+            template: "listDefaultTemplate@templates/lists.html",
+            components: {
+                paging: {
+                    isOn: true,
+                    defaultPageSize: 10,
+                    pageSizes: [10, 25, 50, 100, 250, 500],
+                    pageSizeChangeArea: true,
+                    gotoPageArea: 'combobox', // possible values: 'textbox', 'combobox', 'none'
+                    maxNumberOfAllShownPages: 5,
+                    block1NumberOfPages: 1,
+                    block2NumberOfPages: 5,
+                    block3NumberOfPages: 1
+                },
+                sorting: {
+                    isOn: false,
+                    loadFromLocalStorage: true,
+                    default: {
+                        fieldId: undefined,
+                        type: undefined
+                    },
+                    allowUser: false
+                },
+                filtering: {
+                    isOn: false,
+                    filteringComponentId: 'zcrud-filtering',
+                    elementIdSuffix: '-filter'
+                },
+                selecting: {
+                    isOn: false,
+                    multiple: true,
+                    mode: [ 'checkbox', 'onRowClick' ] // possible values: 'checkbox' and 'onRowClick'
+                },
+                editing: {
+                    isOn: false,
+                    event: 'batch',    // possible values: 'fieldChange', 'batch'
+                    dataToSend: 'all', // possible values: 'modified', 'all',
+                    modifiedFieldsClass: 'zcrud-modified-field',
+                    modifiedRowsClass: 'zcrud-modified-row',
+                    hideTr: function( $tr ){
+                        $tr.fadeOut();
+                    },
+                    showTr: function( $tr ){
+                        $tr.fadeIn();
+                    }
+                }
+            },
+            buttons: {
+                toolbar: {
+                    newRegisterRow: undefined,
+                    openNewRegisterForm: undefined,
+                    undo: undefined,
+                    redo: undefined,
+                    save: undefined
+                },
+                byRow: {
+                    openEditRegisterForm: undefined,
+                    openDeleteRegisterForm: undefined,
+                    deleteRegisterRow: undefined,
+                }
+            }
+        }, create: {
+            template: "formDefaultTemplate@templates/forms.html"
+        }, update: {
+            template: "formDefaultTemplate@templates/forms.html"
+        }, delete: {
+            template: "deleteDefaultTemplate@templates/forms.html"
+        }
+    },
+
+    defaultFormConf: {
+        event: 'batch',    // possible values: 'fieldChange', 'batch'
+        dataToSend: 'all', // possible values: 'modified', 'all',
+        modifiedFieldsClass: 'zcrud-modified-field',
+        modifiedRowsClass: 'zcrud-modified-row',
+        hideTr: function( $tr ){
+            $tr.fadeOut();
+        },
+        showTr: function( $tr ){
+            $tr.fadeIn();
+        },
+        buttons: {
+            toolbar: {
+                undo: true,
+                redo: true,
+                cancel: true
+            },
+            byRow: {
+                openEditRegisterForm: true,
+                openDeleteRegisterForm: true,
+                deleteRegisterRow: true,
+            }
+        }
+    },
+
+    templates: {
+        declaredRemotePageUrls: [],
+        busyTemplate: "busyDefaultTemplate@templates/misc.html"
+    },
+
+    ajax: {
+        ajaxFunction: $.ajax,
+        defaultFormAjaxOptions: {
+            dataType   : 'json',
+            contentType: 'application/json; charset=UTF-8',
+            type       : 'POST'
+        },
+        ajaxPreFilter: function( data ){
+            return data;
+        },
+        ajaxPostFilter : function( data ){
+            return data;
+        }
+    },
+
+    i18n: {
+        language: 'en',
+        filesPath: 'i18n',
+        i18nArrayVarName: 'i18nArray',
+        files: {}
+    },
+    /*
+        serverDataFormat: {
+            datetime: 'm/d/Y H:i', 
+            date: 'm/d/Y',          
+            time: 'H:i',            
+            decimalSeparator: '.'
+        },*/
+
+    logging: {
+        isOn: false,
+        level: log4javascript.Level.ERROR,
+    },
+
+    fatalErrorFunction: function( message ){
+        alert( message );
+    }
+};
---
+++ js/app/normalizer.js
@@ -0,0 +1,164 @@
+/* 
+    normalizer singleton class
+*/
+var context = require( './context.js' );
+var fieldBuilder = require( './fields/fieldBuilder.js' );
+var $ = require( 'jquery' );
+
+module.exports = (function() {
+    "use strict";
+
+    // Normalizes some options (sets default values)
+    var normalize = function( options ) {
+
+        normalizeGeneralOptions( options );
+
+        $.each( options.fields, function ( fieldId, field ) {
+            normalizeFieldOptions( fieldId, field, options );
+        });
+
+        normalizeGeneralOptionsPostFields( options );
+    };
+
+    // Normalizes some general options (non related to fields)
+    var normalizeGeneralOptions = function( options ) {
+
+        if ( options.formId == undefined ){
+            options.formId = 'zcrud-form-' + options.entityId;
+        }
+
+        // Normalize list options
+        var listOptions = options.pages.list;
+        if ( listOptions.formId == undefined ){
+            listOptions.formId = 'zcrud-list-form-' + options.entityId;
+        }
+        if ( listOptions.id == undefined ){
+            listOptions.id = 'zcrud-list-' + options.entityId;
+        }
+        if ( listOptions.tableId == undefined ){
+            listOptions.tableId = 'zcrud-list-table-' + options.entityId;
+        }
+        if ( listOptions.tbodyId == undefined ){
+            listOptions.tbodyId = 'zcrud-list-tbody-' + options.entityId;
+        }
+
+        // Normalize editable list
+        var editableListIsOn = listOptions.components.editing.isOn;
+        var toolbar = listOptions.buttons.toolbar;
+        if ( toolbar.newRegisterRow == undefined ){
+            toolbar.newRegisterRow = editableListIsOn;
+        }
+        if ( toolbar.openNewRegisterForm == undefined ){
+            toolbar.openNewRegisterForm = ! editableListIsOn;
+        }
+        if ( toolbar.undo == undefined ){
+            toolbar.undo = editableListIsOn;
+        }
+        if ( toolbar.redo == undefined ){
+            toolbar.redo = editableListIsOn;
+        }
+        if ( toolbar.save == undefined ){
+            toolbar.save = editableListIsOn;
+        }
+        var byRow = listOptions.buttons.byRow;
+        if ( byRow.openEditRegisterForm == undefined ){
+            byRow.openEditRegisterForm = ! editableListIsOn;
+        }
+        if ( byRow.openDeleteRegisterForm == undefined ){
+            byRow.openDeleteRegisterForm = ! editableListIsOn;
+        }
+        if ( byRow.deleteRegisterRow == undefined ){
+            byRow.deleteRegisterRow = editableListIsOn;
+        }
+    };
+
+    // Normalizes some options for a field (sets default values)
+    var normalizeFieldOptions = function ( id, field, options, parent ) {
+
+        // Set id
+        field.id = id;
+
+        // Set the key
+        if ( field.key ){
+            options.key = id;
+        }
+        field.name = id;
+
+        // Set defaults when undefined
+        if ( field.type == undefined ) {
+            field.type = 'text';
+        }
+        if ( field.elementId == undefined ) {
+            field.elementId = 'zcrud-' + id;
+        }
+        if ( field.elementName == undefined ) {
+            field.elementName = parent? parent.id + context.subformSeparator + id: id;
+        }
+        //field.labelFor = fieldBuilder.getLabelFor( field, options );
+        if ( field.template == undefined ){
+            field.template = fieldBuilder.getTemplate( field, options );
+        }
+        if ( field.viewTemplate == undefined ){
+            field.viewTemplate = fieldBuilder.getViewTemplate( field );
+        }
+        context.declareRemotePageUrl( field.template, options.templates.declaredRemotePageUrls );
+        if ( field.formFieldAttributes == undefined ){
+            field.formFieldAttributes = {};
+        }
+        if ( field.sorting == undefined ){
+            field.sorting = true;
+        }
+
+        // Convert dependsOn to array if it's a comma separated lists
+        if ( field.dependsOn && $.type( field.dependsOn ) === 'string' ) {
+            var dependsOnArray = field.dependsOn.split( ',' );
+            field.dependsOn = [];
+            for ( var i = 0; i < dependsOnArray.length; i++ ) {
+                field.dependsOn.push( $.trim( dependsOnArray[ i ] ) );
+            }
+        }
+
+        // 
+        normalizeCustomOptionsField( field, options );
+
+        // Normalize subfields in this field
+        if ( field.fields ){
+            $.each( field.fields, function ( subfieldId, subfield ) {
+                normalizeFieldOptions( subfieldId, subfield, options, field );
+            });
+        }
+    };
+
+    var normalizeGeneralOptionsPostFields = function( options ) {
+
+        // Add remote page URLs to allDeclaredRemotePageUrls array
+        options.allDeclaredRemotePageUrls = options.templates.declaredRemotePageUrls.slice();
+        context.declareRemotePageUrl( options.templates.busyTemplate, options.allDeclaredRemotePageUrls );
+
+        for ( var i in options.pages ) {
+            var template = options.pages[ i ].template;
+            context.declareRemotePageUrl( template, options.allDeclaredRemotePageUrls );
+        }
+        //alert( JSON.stringify( options.allDeclaredRemotePageUrls ) );
+    };
+
+    var normalizeCustomOptionsField = function( field, options ){
+
+        if ( ! field.customOptions ){
+            field.customOptions = {};
+        }
+
+        var defaultFieldOptions = options.fieldsConfig.defaultFieldOptions[ field.type ];
+        if ( ! defaultFieldOptions ){
+            defaultFieldOptions = {};
+        }
+
+        field.customOptions = $.extend( true, {}, defaultFieldOptions, field.customOptions );
+    };
+
+    
+    return {
+        run: normalize,
+        normalizeFieldOptions: normalizeFieldOptions
+    };
+})();
