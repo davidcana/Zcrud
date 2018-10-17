1bc65a04b2115619f6bbbcf629b7840a8e489294
David Cana <david.javapagetemplates@gmail.com>
2018-10-17 13:25:19 +0200

    Bug fixed: validation.js.

---
+++ docs/js/validation.js
@@ -0,0 +1,219 @@
+//
+
+var options = {
+
+    entityId: 'people',
+    saveUserPreferences: false,
+    
+    pageConf: {
+        defaultPageConf: {
+            url: 'http://localhost/CRUDManager.do?cmd=BATCH_UPDATE&table=people',
+            getRecordURL: 'http://localhost/CRUDManager.do?cmd=GET&table=people'
+        },
+        pages: {
+            list: {
+                url: 'http://localhost/CRUDManager.do?cmd=LIST&table=people',
+                fields: [ 'id', 'name', 'datetime', 'country', 'city', 'browser' ]
+            }, 
+            create: {
+                fields: [
+                    {
+                        "type": "fieldsGroup"
+                    }
+                ]
+            }, 
+            update: {
+                fields: [
+                    {
+                        "type": "fieldsGroup"
+                    }
+                ]
+            }, 
+            delete: {
+                fields: [
+                    {
+                        "type": "fieldsGroup"
+                    }
+                ]
+            }
+        }
+    },
+
+    key : 'id',
+    fields: {
+        id: {
+        },
+        name: {
+            width: '30%'
+        },
+        description: {
+            type: 'textarea',
+            formFieldAttributes: {
+                rows: 6,
+                cols: 80
+            }
+        },
+        date: {
+            type: 'date',
+            customOptions: {
+                inline: false
+            }
+        },
+        time: {
+            type: 'time'
+        },
+        datetime: {
+            type: 'datetime'
+        },
+        phoneType: {
+            type: 'radio',
+            translateOptions: true,
+            options: [
+                { value: '1', displayText: 'homePhone_option' }, 
+                { value: '2', displayText: 'officePhone_option' }, 
+                { value: '3', displayText: 'cellPhone_option' } 
+            ]
+        },
+        country: {
+            type: 'select',
+            translateOptions: false,
+            options: [
+                { value: 1, displayText: 'France' }, 
+                { value: 2, displayText: 'Italy' },
+                { value: 3, displayText: 'Portugal' }, 
+                { value: 4, displayText: 'Spain' }, 
+                { value: 5, displayText: 'UK' }
+            ],
+            defaultValue: '4'
+        },
+        city: {
+            type: 'select',
+            sorting: false,
+            dependsOn: 'country',
+            options: function( data ){
+
+                if ( ! data.dependedValues.country ){
+                    return [];
+                }
+
+                switch ( parseInt( data.dependedValues.country ) ) {
+                    case 1:
+                        return [ 
+                            { value: 1, displayText: 'Paris' }, 
+                            { value: 2, displayText: 'Marseille' }, 
+                            { value: 3, displayText: 'Lyon' }, 
+                            { value: 4, displayText: 'Toulouse' },
+                            { value: 5, displayText: 'Nice' }
+                        ];
+                    case 2:
+                        return [ 
+                            { value: 1, displayText: 'Roma' }, 
+                            { value: 2, displayText: 'Milano' }, 
+                            { value: 3, displayText: 'Napoli' }, 
+                            { value: 4, displayText: 'Torino' },
+                            { value: 5, displayText: 'Paliemmu' }
+                        ];
+                    case 3:
+                        return [ 
+                            { value: 1, displayText: 'Lisboa' }, 
+                            { value: 2, displayText: 'Oporto' }, 
+                            { value: 3, displayText: 'Vila Nova de Gaia' }, 
+                            { value: 4, displayText: 'Amadora' },
+                            { value: 5, displayText: 'Braga' }
+                        ];
+                    case 4:
+                        return [ 
+                            { value: 1, displayText: 'Madrid' }, 
+                            { value: 2, displayText: 'Barcelona' }, 
+                            { value: 3, displayText: 'Valencia' }, 
+                            { value: 4, displayText: 'Sevilla' },
+                            { value: 5, displayText: 'Zaragoza' }
+                        ];
+                    case 5:
+                        return [ 
+                            { value: 1, displayText: 'London' }, 
+                            { value: 2, displayText: 'Birmingham' }, 
+                            { value: 3, displayText: 'Glasgow' }, 
+                            { value: 4, displayText: 'Liverpool' },
+                            { value: 5, displayText: 'Leeds' }
+                        ];
+                    default:
+                        throw 'Unknown country: ' + data.dependedValues.country;
+                }
+            }
+        },
+        browser: {
+            type: 'datalist',
+            options: [ 'Internet Explorer', 'Firefox', 'Chrome', 'Opera', 'Safari' ]
+        },
+        important: {
+            type: 'checkbox'
+        },
+        hobbies: {
+            type: 'checkboxes',
+            translateOptions: true,
+            options: [ 'reading_option', 'videogames_option', 'sports_option', 'cards_option' ]
+        }
+    },
+
+    validation: {
+        modules: 'security, date',
+        rules: {
+            'name': {
+                validation: 'length',
+                length: '3-12'
+            }
+        }
+    },
+    
+    events: {
+        formSubmitting: function ( data, options ) {
+            
+            var isValid = data.$form.find( 'textarea[name="description"]' ).val() != 'forbidden'; 
+            if ( isValid ){
+                return true;
+            }
+
+            return {
+                translate: false,
+                message: 'Forbidden value!'
+            };
+        }
+    },
+    
+    ajax: {
+        ajaxFunction: zcrudServerSide.ajax
+    },
+
+    i18n: {
+        language: 'en',
+        files: { 
+            en: [ 'en-common.json', 'en-people.json' ],
+            es: [ 'es-common.json', 'es-people.json' ] 
+        }
+    }
+};
+
+var zptParser = zpt.buildParser({
+    root: document.body,
+    //root: [ $( '#commonHeader' )[0], $( '#commonFooter' )[0] ],
+    dictionary: {
+        location: window.location
+    },
+    declaredRemotePageUrls: [ 'templates.html' ]
+});
+
+zptParser.init(
+    function(){
+        zptParser.run();
+        
+        $( '#container' ).zcrud( 
+            'init',
+            options,
+            function( options ){
+                $( '#container' ).zcrud( 'renderList' );
+            }
+        );
+    }
+);
+
